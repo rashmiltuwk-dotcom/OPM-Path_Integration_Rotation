716019a2a021bf85c924a7a171525c7b8fea9b6e
rashmil <rashmil.tu.wk@gmail.com>
2026-06-30 10:07:06 +0100

    Update TriggerLoggerPauseFix.m

--- Experiment Files/TriggerLoggerPauseFix.m
+++ Experiment Files/TriggerLoggerPauseFix.m
@@ -47,22 +47,23 @@ end
         
             % Channels 2-7 = binary 01111110 = decimal 126
             allChannels = 126;
+            eventName = 'ExperimentStart';
             
             % Fire channels 2-7 HIGH
             io64(obj.ioObj, obj.address, allChannels);
             
             % Log it
             timestamp = GetSecs - obj.expStartTime;
-            fprintf(obj.logFileID, '%.4f,%d,%s,ALL,126,ON\n', ...
-                timestamp, trueTrial, 'ExperimentStart');
+            fprintf(obj.logFileID, '%.4f,%d,%s,%s,%d,ON\n', ...
+                timestamp, trueTrial, eventName, '2-7', allChannels);
             
             % Brief hold so hardware registers the pulse
             WaitSecs(0.05);
             
             % Drop all channels to LOW
             io64(obj.ioObj, obj.address, 0);
-            fprintf(obj.logFileID, '%.4f,%d,%s,ALL,0,OFF\n', ...
-                GetSecs - obj.expStartTime, trueTrial, 'ExperimentStart');
+            fprintf(obj.logFileID, '%.4f,%d,%s,%s,0,OFF\n', ...
+                GetSecs - obj.expStartTime, trueTrial, eventName, '2-7');
         end
 
         function startEvent(obj, triggerChannel, trueTrial, eventName)
